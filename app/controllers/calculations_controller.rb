f8e6258ce7b42959037e983ce2dee2ddc1f9ce0b
farahschueller <Farah@linux.site>
2015-05-18 17:32:20 +0200

    RESTful routes for calculations, ajaxified pagination

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,17 +1,18 @@
-class CalculatorController < ApplicationController
-  def new
-  	#@calculations = Calculation.last(10)
-  	# if params["first_id"]
-  	# 	@calculations = Calculation.where("id >= #{params['first_id']}").limit(10)
+class CalculationsController < ApplicationController
+  def index
   	@calculations = Calculation.order("created_at" => :desc).page(params[:page]).per(10)
   end
 
   def create
   	@calculation = Calculation.create(equation: params[:q]) # creates calculation object in database
 	flash[:error] ="No not that. No alphabetical characters and avoid spaces, please!" unless @calculation.persisted?
-  	redirect_to new_calculator_path	unless request.xhr?
+  	redirect_to calculations_path	unless request.xhr?
   end
 
-  def enter
+  def destroy 
+    @calculation = Calculation.find(params[:id])
+    flash[:error] = "Record not found in database. Try again." unless @calculation.exists?
+    @calculation.delete
+    redirect_to index_calculator_path
   end
 end
