7ffdde57cd7a96bb8f7a8b34f7f0268b20e0930a
farahschueller <Farah@linux.site>
2015-05-19 14:16:55 +0200

    set up RESTful routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,11 @@ Rails.application.routes.draw do
   # post 'calculator/create'
   # get 'calculator/show'
 
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'calculator#new'
+  root 'calculations#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -18,7 +17,9 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  resources :calculator
+  resources :calculations
+
+  #delete 'calculator/:id', to: 'calculator#destroy'
 
   # Example resource route with options:
   #   resources :products do
