edb474fe7cad8292f7c48bfd487b5f60e7562d20
farahschueller <Farah@linux.site>
2015-05-19 14:17:26 +0200

    replaced custom form with default form

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,11 +1,12 @@
 class CalculationsController < ApplicationController
   def index
   	@calculations = Calculation.order("created_at" => :desc).page(params[:page]).per(10)
+    @new_calculation = Calculation.new
   end
 
   def create
-  	@calculation = Calculation.create(equation: params[:q]) # creates calculation object in database
-	flash[:error] ="No not that. No alphabetical characters and avoid spaces, please!" unless @calculation.persisted?
+  	@calculation = Calculation.create(equation: params[:calculation][:equation]) # creates calculation object in database
+	  flash[:error] ="No not that. No alphabetical characters and avoid spaces, please!" unless @calculation.persisted?
   	redirect_to calculations_path	unless request.xhr?
   end
 
