0ded24146a2ee898616ac14609d83e8e8eadc755
farahschueller <Farah@linux.site>
2015-05-20 17:25:45 +0200

    proper rendering of database records for all actions

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,17 +2,34 @@ class CalculationsController < ApplicationController
   def index
   	@calculations = Calculation.order("created_at" => :desc).page(params[:page]).per(10)
     @new_calculation = Calculation.new
+
+    if request.xhr?
+      render "update_table"
+    end
+
   end
 
   def create
   	@calculation = Calculation.create(equation: params[:calculation][:equation]) # creates calculation object in database
 	  flash[:error] ="No not that. No alphabetical characters and avoid spaces, please!" unless @calculation.persisted?
-  	redirect_to calculations_path	unless request.xhr?
+    @calculations = Calculation.order("created_at" => :desc).page(1).per(10)
+
+    if request.xhr?
+      render "update_table"
+    else
+  	  redirect_to calculations_path
+    end
   end
 
   def destroy 
     @calculation = Calculation.find(params[:id])
     @calculation.destroy!
-    redirect_to calculations_path unless request.xhr?
+    @calculations = Calculation.order("created_at" => :desc).page(1).per(10)
+    
+    if request.xhr?
+      render "update_table"
+    else
+      redirect_to calculations_path
+    end
   end
 end
