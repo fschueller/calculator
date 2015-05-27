0e6a1bf6240fc08fcee33c05f8a714cc89561a4b
farahschueller <Farah@linux.site>
2015-05-27 14:35:59 +0200

    draft spec outline for Calculation

--- spec/models/calculation_spec.rb
+++ spec/models/calculation_spec.rb
@@ -1,5 +1,19 @@
-require './rails_helper'
+require 'rails_helper'
 
 RSpec.describe Calculation, type: :model do
-  it "creates valid calculation"
+	describe "equation" do
+	  it "should have presence validated"
+	  it "should have format validated"
+	  it "should be saved to database"
+	end
+
+	describe "evaluation" do
+  	it "should happen automatically"
+  	it "should be correct"
+  end
+
+  describe "result" do
+  	it "should be stored in database"
+  	it "should have presence validated"
+  end
 end
