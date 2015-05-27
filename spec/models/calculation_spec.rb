a2ef73c37bef185e04a1c5f584c222c7a956371e
farahschueller <Farah@linux.site>
2015-05-27 15:05:00 +0200

    implemented tests for equation field

--- spec/models/calculation_spec.rb
+++ spec/models/calculation_spec.rb
@@ -2,9 +2,34 @@ require 'rails_helper'
 
 RSpec.describe Calculation, type: :model do
 	describe "equation" do
-	  it "should have presence validated"
-	  it "should have format validated"
-	  it "should be saved to database"
+	  it "should have presence validated" do
+	  	calculation = Calculation.new
+
+	  	calculation.equation = nil
+	  	expect(calculation).not_to be_valid
+
+	  	calculation.equation = '1'
+	  	expect(calculation).to be_valid
+	  end
+
+	  it "should have format validated" do
+	  	calculation = Calculation.new
+
+	  	calculation.equation = "bjkiuzkjugfnfchjz65ktiukhjggdrz54"
+	  	expect(calculation).not_to be_valid
+
+	  	calculation.equation = "(2+2.8)*3/4-5"
+	  	expect(calculation).to be_valid
+
+	  end
+
+
+	  it "should be saved to database" do
+	  	Calculation.create(equation: "2+2")
+
+	  	expect(Calculation.last.equation).to eq "2+2"
+
+	  end
 	end
 
 	describe "evaluation" do
